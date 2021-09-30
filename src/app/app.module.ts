e40fd5f775f3069f65cb5951152cb1e8d050ea8c
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2021-09-30 14:15:49 +0200

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
