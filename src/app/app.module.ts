42c6aead6d3f1e088c95d50b4b4ec515ecd88600
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2021-09-30 11:49:52 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ServeselementComponent } from './serveselement/serveselement.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    ServeselementComponent,
-    CockpitComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
